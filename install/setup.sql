c632be96a08ed4fe9032996194c78a74df4a1568
André Freitas <p.andrefreitas@gmail.com>
2013-04-04 18:43:17 +0100

    Criado projeto de eclipse com o sql da criação da base de dados e exemplo de dados.

---
+++ install/setup.sql
@@ -0,0 +1,142 @@
+DROP TABLE IF EXISTS orders_products;
+DROP TABLE IF EXISTS invoice;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS products_subscriptions;
+DROP TABLE IF EXISTS favorites;
+DROP TABLE IF EXISTS products_scores;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS products_images;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS stores_files;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS stores_users;
+DROP TABLE IF EXISTS stores;
+DROP TABLE IF EXISTS files;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS privileges;
+
+CREATE TABLE privileges(
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE users(
+     id SERIAL PRIMARY KEY,
+     name TEXT NOT NULL,
+     email TEXT NOT NULL,
+     password TEXT NOT NULL,
+     privilege_id INTEGER REFERENCES privileges(id) NOT NULL
+);
+
+CREATE TABLE files(
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	path TEXT NOT NULL
+);
+
+CREATE TABLE stores(
+	id SERIAL PRIMARY KEY,
+	name TEXT UNIQUE NOT NULL,
+	slogan TEXT NOT NULL,
+	vat INTEGER NOT NULL,
+	logo_id INTEGER REFERENCES files(id),
+	css_id INTEGER REFERENCES files(id)
+);
+
+CREATE TABLE stores_users(
+	user_id INTEGER REFERENCES users(id) NOT NULL,
+	store_id INTEGER REFERENCES stores(id) NOT NULL,
+	PRIMARY KEY (user_id,store_id)
+);
+
+CREATE TABLE categories(
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	store_id INTEGER REFERENCES stores(id) NOT NULL,
+	image_id INTEGER REFERENCES files(id)
+);
+
+CREATE TABLE stores_files(
+	store_id INTEGER REFERENCES stores(id),
+	file_id INTEGER REFERENCES files(id),
+	PRIMARY KEY (store_id,file_id)
+);
+
+CREATE TABLE products(
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	description TEXT NOT NULL,
+	base_cost NUMERIC,
+	stock INTEGER NOT NULL,
+	score INTEGER,
+	category_id INTEGER REFERENCES categories(id)
+);
+
+
+CREATE TABLE products_images(
+	file_id INTEGER REFERENCES files(id),
+	product_id INTEGER REFERENCES products(id),
+	PRIMARY KEY (file_id,product_id)
+);
+
+CREATE TABLE transactions(
+	id SERIAL PRIMARY KEY,
+	transaction_date DATE NOT NULL,
+	ammount INTEGER NOT NULL,
+	description TEXT,
+	store_id INTEGER REFERENCES stores(id)
+);
+
+CREATE TABLE comments(
+	id SERIAL PRIMARY KEY,
+	comment_date DATE NOT NULL,
+	body TEXT NOT NULL,
+	user_id INTEGER references users(id) NOT NULL,
+	product_id INTEGER references products(id) NOT NULL
+);
+
+CREATE TABLE products_scores(
+	user_id INTEGER REFERENCES users(id),
+	product_id INTEGER REFERENCES products(id),
+	score INTEGER NOT NULL CHECK (score>=0 and score<=5),
+	PRIMARY KEY(user_id,product_id)
+);
+
+CREATE TABLE favorites(
+	user_id INTEGER REFERENCES users(id),
+	product_id INTEGER REFERENCES products(id),
+	PRIMARY KEY(user_id,product_id)
+);
+
+CREATE TABLE products_subscriptions(
+	user_id INTEGER REFERENCES users(id),
+	product_id INTEGER REFERENCES products(id),
+	subscription_date DATE NOT NULL,
+	PRIMARY KEY(user_id,product_id)
+);
+
+CREATE TABLE orders(
+	id SERIAL PRIMARY KEY,
+	order_date DATE NOT NULL,
+	paid BOOLEAN NOT NULL,
+	costumer_id INTEGER REFERENCES users(id) NOT NULL,
+	transaction_id INTEGER REFERENCES transactions(id)
+);
+
+CREATE TABLE invoice(
+	id SERIAL PRIMARY KEY,
+	code TEXT UNIQUE NOT NULL,
+	order_date DATE NOT NULL,
+	total numeric NOT NULL,
+	vat numeric NOT NULL,
+	order_id INTEGER REFERENCES orders(id) NOT NULL
+);
+
+CREATE TABLE orders_products(
+	order_id INTEGER REFERENCES orders(id),
+	product_id INTEGER REFERENCES products(id),
+	quantity INTEGER NOT NULL,
+	base_cost NUMERIC NOT NULL,
+	PRIMARY KEY (order_id,product_id)
+);
