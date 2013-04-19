b9572f29fe07a8833bbd73a0d4348c6f4a44cb24
ei10108 <ei10108@I00CN1103.salas.feupsig.fe.up.pt>
2013-04-19 16:33:17 +0100

    coisas na plataforma

--- sql/plataform.sql
+++ sql/plataform.sql
@@ -1,1 +1,50 @@
 /* SQL for the pages of the plataform module */
+
+-- #P301 - Home
+
+
+-- #P302 - Login
+-- param(email, password)
+SELECT id
+FROM users
+WHERE email='my@email.com' and password='dnsajkndklsa';
+
+-- #P303 - Administration
+
+
+-- #P304 - About
+
+
+-- #P305 - Stores
+SELECT *
+FROM stores;
+
+-- #P306 - Account
+
+
+-- #P307 - Update Account
+
+
+-- #P308 - Add Store
+
+
+-- #P309 - Update Store
+
+
+-- #P310 - Delete Store
+
+
+-- #P311 - Merchants
+
+
+-- #P312 - Add Merchant
+
+
+-- #P313 - Update Merchant
+
+
+-- #P314 - Delete Merchant
+
+
+-- #P315 - Register
+
