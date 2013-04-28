a1ef6857d13443a3ae9405e2a03ae59712fafe3f
André Freitas <p.andrefreitas@gmail.com>
2013-04-28 18:19:13 +0100

    Quando não é admin, na página merchants redirecciona para a home.

--- pages/merchants.php
+++ pages/merchants.php
@@ -1,6 +1,9 @@
 <?php
     require_once('../common/init.php');
     require_once('../database/plataform.php');
+    if(!isset($_SESSION['permission']) or $_SESSION['permission']!='admin'){
+        header("Location: index.php");
+    }
     $smarty->assign('BASE_URL',$BASE_URL);
     $smarty->assign('title','Merchants');
     $smarty->assign('loggedin',isset($_SESSION['id']));
