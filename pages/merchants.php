68cc204b0b1b785304b7dd8995c6a14d73771b13
André Freitas <p.andrefreitas@gmail.com>
2013-05-03 11:30:47 +0100

    Corrigido o header do merchants.php

--- pages/merchants.php
+++ pages/merchants.php
@@ -1,9 +1,10 @@
 <?php
-    require_once('../common/init.php');
-    require_once('../database/plataform.php');
     if(!isset($_SESSION['permission']) or $_SESSION['permission']!='admin'){
         header("Location: index.php");
     }
+    require_once('../common/init.php');
+    require_once('../database/plataform.php');
+
     $smarty->assign('BASE_URL',$BASE_URL);
     $smarty->assign('title','Merchants');
     $smarty->assign('loggedin',isset($_SESSION['id']));
