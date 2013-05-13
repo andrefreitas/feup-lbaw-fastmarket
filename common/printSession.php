fa894c2e9c4f34e510615c9a43950da1842ca564
André Freitas <p.andrefreitas@gmail.com>
2013-05-13 16:01:28 +0100

    Alterado o printSession.

--- common/printSession.php
+++ common/printSession.php
@@ -1,7 +1,5 @@
 <?php
 /* Outputs the session in raw mode */
-require_once(dirname(__FILE__) . '/configuration.php');
-session_set_cookie_params (0, $COOKIE_PATH);
-session_start();
+require_once(dirname(__FILE__) . '/init.php');
 print_r($_SESSION);
 ?>
