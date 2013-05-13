7b337f96c5236171ac07a7f4357a87d082ab3094
André Freitas <p.andrefreitas@gmail.com>
2013-05-13 16:32:12 +0100

    A corrigir o cookie path para ser automático.

--- common/configuration.php
+++ common/configuration.php
@@ -4,7 +4,24 @@
 	$dbPassword = 'nB50vy';
 	$dbSchema = 'public';
 	$host = 'vdbm.fe.up.pt';
+	
+	/* Compute Base Path */
 	$BASE_PATH = explode("common", dirname(__FILE__) );
 	$BASE_PATH = $BASE_PATH[0];
-	$COOKIE_PATH = $BASE_PATH;
+	
+
+	$ROOT_FOLDER = explode($BASE_PATH, "/");
+	print_r($BASE_PATH);
+
+	/* Compute Cookie Path */
+	$COOKIE_PATH = explode("common", dirname(__FILE__) );
+	$COOKIE_PATH = $COOKIE_PATH[0];
+	$COOKIE_PATH = rtrim($COOKIE_PATH, "/");
+	$COOKIE_PATH = explode("/", $COOKIE_PATH );
+	unset($COOKIE_PATH[0]);
+	$COOKIE_PATH = implode("/", $COOKIE_PATH);
+	$COOKIE_PATH = '/' . $COOKIE_PATH . '/';	
+	
+	//echo $_SERVER['REQUEST_URI'];
+	
 ?>
