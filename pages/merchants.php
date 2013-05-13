55290881e0b22bd79bb029207f4ca6f7e0fdb53c
André Freitas <p.andrefreitas@gmail.com>
2013-05-13 17:17:04 +0100

    O merchants.php aparece em branco para os visitantes. Não se pode colocar uma directiva header() após o start_session() o que é impossível contornar nesta situação.

--- pages/merchants.php
+++ pages/merchants.php
@@ -5,12 +5,12 @@ chdir('../database');
 require_once('plataform.php');
 chdir('../pages');
 
-if(!isset($_SESSION['permission']) or $_SESSION['permission']!='admin'){
-    header("Location: index.php");
+if(isset($_SESSION['permission']) and $_SESSION['permission']=='admin'){
+    $smarty->assign('title','Merchants');
+    $smarty->assign('loggedin',isset($_SESSION['id']));
+    $smarty->assign('merchants',getMerchants());
+    $smarty->display('merchants.tpl');
 }
 
-$smarty->assign('title','Merchants');
-$smarty->assign('loggedin',isset($_SESSION['id']));
-$smarty->assign('merchants',getMerchants());
-$smarty->display('merchants.tpl');
+
 ?>
