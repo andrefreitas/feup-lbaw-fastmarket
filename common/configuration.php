8c24976240991fcdd131354609f7a20085607232
André Freitas <p.andrefreitas@gmail.com>
2013-05-13 17:01:09 +0100

    Cookie path finalmente a funcionar!

--- common/configuration.php
+++ common/configuration.php
@@ -9,19 +9,15 @@
 	$BASE_PATH = explode("common", dirname(__FILE__) );
 	$BASE_PATH = $BASE_PATH[0];
 	
+    /* Compute root folder */
+	$ROOT_FOLDER = str_replace("\\", "/", $BASE_PATH);
+    $ROOT_FOLDER = rtrim($ROOT_FOLDER, "/");
+    $ROOT_FOLDER = explode("/", $ROOT_FOLDER);
+	$ROOT_FOLDER = end($ROOT_FOLDER);
 
-	$ROOT_FOLDER = explode($BASE_PATH, "/");
-	print_r($BASE_PATH);
-
-	/* Compute Cookie Path */
-	$COOKIE_PATH = explode("common", dirname(__FILE__) );
-	$COOKIE_PATH = $COOKIE_PATH[0];
-	$COOKIE_PATH = rtrim($COOKIE_PATH, "/");
-	$COOKIE_PATH = explode("/", $COOKIE_PATH );
-	unset($COOKIE_PATH[0]);
-	$COOKIE_PATH = implode("/", $COOKIE_PATH);
-	$COOKIE_PATH = '/' . $COOKIE_PATH . '/';	
 	
-	//echo $_SERVER['REQUEST_URI'];
+	/* Compute Cookie Path */
+    $COOKIE_PATH = explode($ROOT_FOLDER, $_SERVER['REQUEST_URI']);
+	$COOKIE_PATH = $COOKIE_PATH[0] . $ROOT_FOLDER . "/";	
 	
 ?>
