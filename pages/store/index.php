e3bf702ad4ae529134a54542542a5c3e9477e3eb
André Freitas <p.andrefreitas@gmail.com>
2013-05-27 15:21:59 +0100

    Colocada a página com logo

--- pages/store/index.php
+++ pages/store/index.php
@@ -8,18 +8,26 @@ chdir('../pages/store');
 if(!isset($_GET["store"]) or !storeExists($_GET["store"])){
     header("Location: ../index.php");
 }
-$domain = $_GET["store"];
+
+
 /* BEGIN -- Get store data */
+$domain = $_GET["store"];
+$storeId = getStoreId($domain);
+
 // Logo
 $logoPath = "../../files/" . getStoreLogo($domain);
-
 // Categories
+$categories = getCategories($storeId);
 // Products
+$products = getStoreProductsOnStock($storeId, 10);
+//print_r($products);
 /* END -- Get store data */
 
 
 $smarty->assign('title', "Welcome");
 $smarty->assign('logoPath', $logoPath);
+$smarty->assign('categories', $categories);
+$smarty->assign('products', $products);
 $smarty->display('store/home.tpl');
 
 ?>
--- templates/store/home.tpl
+++ templates/store/home.tpl
@@ -4,16 +4,21 @@
 	<div class="container-fluid">
 		<div class="row-fluid">
 			<div class="span2">
+			    <!--  Categories  -->
 				<ul class="nav nav-list">
 					<li class="nav-header">Categories</li>
-					<li><a href="#">Sport</a></li>
-					<li><a href="#">Bike</a></li>
-
+					{foreach from=$categories item=category}
+					    <li><a href="#">{$category.name}</a></li>
+                    {/foreach}
 				</ul>
+				<!--  New Registration -->
+			
 			</div>
 			<div class="span10">
 				<!--Body content-->
-				
+				{foreach from=$products item=product}
+					    <img src="..." class="img-polaroid">
+                {/foreach}
 			</div>
 		</div>
 	</div>
