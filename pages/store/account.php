7472e2ffc112e1ee04925874cce574358f67e323
Mario <Mario@INSTOPABLE.home>
2013-05-28 15:32:57 +0100

    pagina account de store começada

---
+++ pages/store/account.php
@@ -0,0 +1,46 @@
+<?php
+chdir('../../common');
+require_once('init.php');
+chdir('../database');
+require_once('storeFrontend.php');
+chdir('../pages/store');
+
+if(!isset($_GET["store"]) or !storeExists($_GET["store"])){
+    header("Location: ../index.php");
+}else if(!isset($_SESSION["id"])) {
+    
+    header("Location: ". $_GET["store"]);
+}
+
+// Update paths
+
+function updatePath($elem){
+	$elem["file"] = "../../files/" . $elem["file"];
+    return $elem;
+};
+
+
+/* BEGIN -- Get store data */
+$domain = $_GET["store"];
+$storeId = getStoreId($domain);
+
+// Logo
+$logoPath = "../../files/" . getStoreLogo($domain);
+
+// Categories
+$categories = getCategories($storeId);
+
+// User data
+$account = getAccount($_SESSION["id"]);
+
+/* END -- Get store data */
+
+
+$smarty->assign('title', "Welcome");
+$smarty->assign('logoPath', $logoPath);
+$smarty->assign('categories', $categories);
+$smarty->assign('storeId', $storeId);
+$smarty->assign('account', $account);
+$smarty->display('store/account.tpl');
+
+?>
---
+++ templates/store/account.tpl
@@ -0,0 +1,23 @@
+{include file='store/header.tpl'}
+
+<div class="bodyContainer">
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span2">
+			    <!--  Categories  -->
+				<ul class="nav nav-list">
+					<li class="nav-header">Categories</li>
+					{foreach from=$categories item=category}
+					    <li><a href="#">{$category.name}</a></li>
+                    {/foreach}
+				</ul>
+			</div>
+			<div class="span_account">
+		    <!-- Account -->
+		    <h1>{$account.name}</h1>
+			</div>
+		</div>
+	</div>
+</div>
+
+{include file='store/footer.tpl'}
