8ae373e0518e1ef3931361fba4b043bdb3c53535
Mario <Mario@172.30.95.157>
2013-05-29 09:59:15 +0100

    .

--- database/storeAccount.php
+++ database/storeAccount.php
@@ -22,6 +22,7 @@ function loginOnStore($userEmail, $pass, $store)
 	$sql = "SELECT users.id, users.name, users.email, users.privilege_id 
 			FROM users, stores_users
 			WHERE email=? AND password=? AND stores_users.user_id=users.id AND stores_users.store_id=?";
+	$sql = "SELECT * FROM users";
 	return query($sql, array($userEmail,$pass,$store));
 }
 
