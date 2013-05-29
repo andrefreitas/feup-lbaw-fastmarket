4f69eb4df258e9114c7019673ed2efcd2a759a37
Mario <Mario@172.30.95.157>
2013-05-29 10:27:02 +0100

    .

--- ajax/store/addCostumer.php
+++ ajax/store/addCostumer.php
@@ -3,7 +3,9 @@ chdir("../../database");
 require_once("storeAccount.php");
 
 if(isset($_GET["storeId"]) and isset($_GET["name"]) and isset($_GET["email"]) and isset($_GET["password"])){
-    addCostumer($_GET["storeId"], $_GET["name"], $_GET["email"], $_GET["password"]);
+
+	$password = hash('sha256',$_GET['password']);
+    addCostumer($_GET["storeId"], $_GET["name"], $_GET["email"], $password);
     echo json_encode(array("result" => "ok"));
 }else{
     echo json_encode(array("result" => "missingParams"));
