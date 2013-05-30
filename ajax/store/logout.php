6ffed1fd437689f48da6c09de93261155a1773ce
Mario <Mario@INSTOPABLE.home>
2013-05-30 18:10:58 +0100

    .

--- ajax/store/logout.php
+++ ajax/store/logout.php
@@ -2,13 +2,11 @@
 header('Content-type: application/json');
 chdir('../../common');
 require_once('init.php');
-chdir('../database');
-require_once('storeAccount.php');
+
 chdir('../ajax/store');
 
-if (isset($_GET['storeId'])) {
-    
-	    $_SESSION['storesLogin'][$_GET['storeId']]['userId']=null;
-	    echo json_encode(Array("result"=>"ok"));
-  }
+
+$_SESSION['storesLogin'][$_GET['storeId']]['userId']=null;
+echo json_encode(Array("result"=>"ok"));
+ 
 ?>
