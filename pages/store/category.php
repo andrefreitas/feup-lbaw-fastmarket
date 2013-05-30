520f2a2a15b520c28e06ae24843b9d8d208ff81f
Mario <Mario@INSTOPABLE.home>
2013-05-30 17:33:08 +0100

    adicionada pagina de categoria

---
+++ pages/store/category.php
@@ -0,0 +1,44 @@
+<?php
+chdir('../../common');
+require_once('init.php');
+chdir('../database');
+require_once('storeFrontend.php');
+chdir('../pages/store');
+
+if(!isset($_GET["store"]) or !storeExists($_GET["store"]) or
+   !isset($_GET["categoryid"]) ){
+    header("Location: ../index.php");
+}
+
+// Update paths
+function updatePath($elem){
+	$elem["file"] = "../../files/" . $elem["file"];
+    return $elem;
+};
+
+/* BEGIN -- Get store data */
+$domain = $_GET["store"];
+$storeId = getStoreId($domain);
+
+// Logo
+$logoPath = "../../files/" . getStoreLogo($domain);
+
+// Categories
+$categories = getCategories($storeId);
+
+// Products
+$products = getProductsOfCategory($_GET["categoryid"]);
+$products = array_map("updatePath", $products);
+
+
+/* END -- Get store data */
+
+
+$smarty->assign('title', "Welcome");
+$smarty->assign('logoPath', $logoPath);
+$smarty->assign('categories', $categories);
+$smarty->assign('products', $products);
+$smarty->assign('storeDomain', $domain);
+$smarty->assign('storeId', $storeId);
+$smarty->display('store/home.tpl');
+?>
