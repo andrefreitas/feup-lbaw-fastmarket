07cd23f5e3b6137f539bb182e267608580e225cb
Mario <Mario@INSTOPABLE.home>
2013-06-01 19:53:40 +0100

    .

--- javascript/store/frontend.js
+++ javascript/store/frontend.js
@@ -89,7 +89,7 @@ function updateAccount(){
 	if(ret=="ok")
 	{
 		var storeDomain = $("#registerForm #storeDomain").val();
-		window.location= "account.php?&store=" + storeDomain;
+		window.location.href= "account.php?&store=" + storeDomain;
 		
 	}else{
 		alert("erro: "+ret);
