bc285fdf11c91fd49b6ee3d615f28bd84de32f3e
Mario <Mario@172.30.78.177>
2013-06-03 15:14:38 +0100

    .

--- javascript/store/frontend.js
+++ javascript/store/frontend.js
@@ -314,7 +314,7 @@ function addCategory(){
 	{
 		var storeDomain = $("#registerForm #storeDomain").val();
 		
-		window.location.assign= "account.php?store=" + storeDomain;
+		window.location.pathname= "fm/pages/store/account.php?store=" + storeDomain;
 		
 		//document.location.reload(true);
 	}else{
